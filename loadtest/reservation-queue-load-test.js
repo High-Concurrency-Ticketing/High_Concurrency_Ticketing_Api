0024674caa1920b5ae680786149d1daf854888eb
sunjaewow <chltjswo789@naver.com>
2026-09-17 12:28:59 +0900

    test : version 추가

--- loadtest/reservation-queue-load-test.js
+++ loadtest/reservation-queue-load-test.js
@@ -3,8 +3,9 @@ import {check, sleep} from 'k6';
 import exec from 'k6/execution';
 
 const CONCERT_ID = 1;
+const VU = 50000;
 const NODE_INDEX = Number(__ENV.K6_NODE_INDEX || 0);
-const USERS_PER_NODE = Number(__ENV.USERS_PER_NODE || 25000);
+const USERS_PER_NODE = Number(__ENV.USERS_PER_NODE || VU);
 const START_AT = Number(__ENV.START_AT || 0);
 const EC2_IP = String(__ENV.EC2_IP || 'localhost');
 
@@ -12,7 +13,7 @@ export const options = {
     scenarios: {
         measure: {
             executor: 'per-vu-iterations',
-            vus: 25000,
+            vus: VU,
             iterations: 1,
         },
     },
@@ -30,7 +31,7 @@ export default function () {
     waitUntilStartAt();
 
     const userId = NODE_INDEX * USERS_PER_NODE + exec.scenario.iterationInTest + 1;
-    const url = `http://${EC2_IP}:8080/reservation-queues/${CONCERT_ID}/users/${userId}`;
+    const url = `http://${EC2_IP}:8080/v1/reservation-queues/${CONCERT_ID}/users/${userId}`;
 
     const response = http.post(url, null, {
         tags: {
